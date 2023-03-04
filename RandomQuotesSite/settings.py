f79fa3c92d74f867fedbe76806c8fa3cc51c59c2
Ababil Mustax <iemmustaq@gmail.com>
2023-03-05 06:08:04 +0700

    fix all error

--- RandomQuotesSite/settings.py
+++ RandomQuotesSite/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR / 'static/')
 
 STATICFILES_DIRS = [BASE_DIR / 'Quotes/static']
 
